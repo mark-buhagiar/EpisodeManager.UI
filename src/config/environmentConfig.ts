cbc4c2626a3ad8dba25f534ae25759324bcf8370
Mark Buhagiar <markbuh25@gmail.com>
2020-07-14 09:51:33 +0100

    Changes form live environ

--- src/config/environmentConfig.ts
+++ src/config/environmentConfig.ts
@@ -1,5 +1,5 @@
 const EnvironmentConfig = {
-    isMocking: true,
+    isMocking: false,
     isAuthenticated: true,
     loading: false, // Only used when isAuthed is set to false
     permissions: ['read:admin', 'execute:admin'],
